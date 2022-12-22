12dfa3f8bf9f10484e6866adc7de06dfc8183166
Ozan Tekin <ardilozantekin@gmail.com>
2022-12-22 14:03:19 +0300

    update

--- src/routes/Contact/Contact.jsx
+++ src/routes/Contact/Contact.jsx
@@ -4,7 +4,7 @@ export default function Contact() {
   return (
     <div className='contact'>
       <header className='contact-header'>
-        <h1> Contact Page </h1>
+        <h1> Contact Page! </h1>
       </header>
     </div>
   )
